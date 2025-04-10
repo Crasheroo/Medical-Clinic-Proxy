7ae47a1f213c71c362c5df735017be4be6f09e1c
Crasheroo <pawelzal43@gmail.com>
2025-04-10 18:47:12 +0200

    First

---
+++ src/main/java/com/example/Mecial_Clinic_Proxy/MecialClinicProxyApplication.java
@@ -0,0 +1,14 @@
+package com.example.Mecial_Clinic_Proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class MecialClinicProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecialClinicProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mecial_Clinic_Proxy/MecialClinicProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mecial_Clinic_Proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecialClinicProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
