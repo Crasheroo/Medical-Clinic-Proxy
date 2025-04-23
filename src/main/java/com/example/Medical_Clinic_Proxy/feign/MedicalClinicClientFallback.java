31e5bd9f7b25b272fb98d313632e4ceaa8f03a45
Crasheroo <pawelzal43@gmail.com>
2025-04-23 16:41:24 +0200

    Fallbacks

--- src/main/java/com/example/Medical_Clinic_Proxy/feign/MedicalClinicClientFallback.java
+++ src/main/java/com/example/Medical_Clinic_Proxy/feign/MedicalClinicClientFallback.java
@@ -12,14 +12,18 @@ import org.springframework.stereotype.Component;
 public class MedicalClinicClientFallback implements MedicalClinicClient {
     @Override
     public PageableContentDTO<Visit> getVisits(VisitFilterDTO filter, Pageable pageable) {
-        return null;
+        log.warn("Fallback: Returning empty list");
+        return new PageableContentDTO<>();
     }
 
     @Override
     public Visit reserveVisit(Long id, String patientEmail) {
-        return null;
+        log.error("Fallback: Could not reserve visit {} for patient {}", id, patientEmail);
+        throw new RuntimeException("Service error");
     }
 
     @Override
-    public void cancelVisit(Long id, String doctorEmail) {}
+    public void cancelVisit(Long id, String doctorEmail) {
+        log.error("Fallback: Could not cancel visit {} for doctor {}", id, doctorEmail);
+    }
 }
